8a75ab22ad8dce26c1f54c0cf159ebcd06850ff7
daall-01 <daall@8699>
2023-05-11 11:08:48 +0900

    join

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,11 +1,21 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @GetMapping({"", "/"})
     public String index(){
         System.out.println("111111");
@@ -23,16 +33,22 @@ public class IndexController {
     public @ResponseBody String manager(){
         return "manager";
     }
-    @GetMapping({"/login"})
-    public @ResponseBody String login(){
-        return "login";
+    @GetMapping({"/loginForm"})
+    public String loginForm(){
+        return "loginForm";
     }
-    @GetMapping({"/join"})
-    public @ResponseBody String join(){
-        return "join";
+    @GetMapping({"/joinForm"})
+    public String joinForm(){
+        return "joinForm";
     }
-    @GetMapping({"/joinProc"})
-    public @ResponseBody String joinProc(){
-        return "회원가입 완료!";
+    @PostMapping({"/join"})
+    public String join(User user){
+        System.out.println(user);
+        user.setRole("ROLE_USER");
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        userRepository.save(user);
+        return "redirect:/loginForm";
     }
 }
